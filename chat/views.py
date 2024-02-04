4fdaf637fb8e451f2fc9b432aa6c958b3f5795ae
Yazdan abbasi <yabasi891@gmail.com>
2024-02-04 19:38:59 +0330

    Added functions in views to check user details. Connected messages to te datbase so messages are now visible in the admin panel

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -9,8 +10,14 @@ def home(request):
 
 
 def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
 
-    return render(request, 'room.html')
+    return render(request, 'room.html', {
+        'username': username,
+        'room': 'room',
+        'room_details': room_details,
+    })
 
 
 def checkview(request):
@@ -24,3 +31,14 @@ def checkview(request):
         new_room = Room.objects.create(name=room_name)
         new_room.save()
         return redirect('/' + room_name + '/?username=' + username)
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(msg=message, user=username, room=room_id)
+    new_message.save()
+
+    return HttpResponse('Message sent successfully')
